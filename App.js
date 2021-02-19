01d080914edc2eaf75ef7ea0bdf7cc54d795957a
Rido Mulanda <ridomulanda@gmail.com>
2021-02-19 15:51:39 +0700

    Basic Styling

--- App.js
+++ App.js
@@ -1,6 +1,38 @@
-import React, { Component, Profiler } from 'react';
-import {Image, Text, TextInput, View} from 'react-native';
+import React, { Component } from 'react';
+import {Image, Text, TextInput, View, StyleSheet} from 'react-native';
+import laptop from './laptop.jpg';
 const App = () =>{
+    return <StylingReactNativeComponent />
+};
+
+const StylingReactNativeComponent = () => {
+    return (
+        <View>
+            {/* <Text style={style.text}> Hello</Text>
+            <View style={{width:100,height:100,backgroundColor:'#FFD500',borderWidth:2,marginTop:20,marginLeft:20}}></View> */}
+            <View style={{padding:12,backgroundColor:'#F2F2F2',width:212,marginTop:10, marginLeft:10, borderRadius:8}}>
+                <Image source={laptop} style={{resizeMode:'cover',width:null,height:100, borderRadius:8}}/>
+                <Text style={{fontSize:14, fontWeight:'bold', marginTop:16}}>New Macbook Pro 2019</Text>
+                <Text style={{fontSize:12,fontWeight:'bold',color:'#F2994A',marginTop:12}}>Rp. 25.000.000</Text>
+                <Text style={{fontSize:12,fontWeight:'300',marginTop:12}}>Jakarta Barat</Text>
+                <View style={{marginTop:20,backgroundColor:'#6FCF97',paddingVertical:6,borderRadius:25}}>
+                    <Text style={{fontSize:14,fontWeight:'600',color:'white', textAlign:'center'}}t>Beli</Text>
+                </View>
+            </View>
+        </View>
+    );
+}
+
+const style = StyleSheet.create({
+    text:{
+        fontSize:20,
+        fontWeight:'bold',
+        marginTop:20,
+        marginLeft:20
+    }
+});
+
+const SampleComponent = () => {
     return (
         <View>
             <HomeHeader />
@@ -12,9 +44,9 @@ const App = () =>{
             <TextInput style={{borderWidth:1}}/>
             <Profile />
         </View>
-    );
-};
-
+    
+    )
+}
 const HomeHeader = () => {
     return <View style={{width:600,height:40,backgroundColor:'#FFD500'}} ></View>;
 };
